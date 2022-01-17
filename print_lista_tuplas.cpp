4ad19f6154efff611e6f82b13f615f0f5a1147d0
viniciuslettieri <viniciuslettieri@yahoo.com.br>
2022-01-16 21:16:25 -0300

    Changing the pointer to a shared_pointer

--- print_lista_tuplas.cpp
+++ print_lista_tuplas.cpp
@@ -1,18 +1,36 @@
 #include <initializer_list>
 #include <iostream>
+#include <memory>
 
 using namespace std;
-
 
 class AbstractPair {
     public:
         virtual void imprime( std::ostream& o ) = 0;
+        virtual ~AbstractPair() { };
 };
 
 template <typename A, typename B>
 class ImplPair : public AbstractPair {
     public:
-        ImplPair( A a, B b ) : a(a), b(b) {}
+        // Constructor
+        ImplPair( A& a, B& b ) : a(a), b(b) { }
+
+        // Copy Constructor
+        ImplPair( const ImplPair& par ) {
+            a = par.a;
+            b = par.b;
+        }
+
+        // Move Constructor
+        ImplPair( ImplPair&& par ) : a(par.a), b(par.b) { }
+
+        // Move Assignment
+        const ImplPair& operator = ( ImplPair&& par ) {
+            a = par.a;
+            b = par.b;
+            return *this;
+        }
 
         void imprime( std::ostream& o ) {
             o << a << " = " << b << std::endl;
@@ -25,29 +43,29 @@ class ImplPair : public AbstractPair {
 
 class Pair {
     public:
+        // Constructor
         template <typename A, typename B>
         Pair( A a, B b ) {
-            p = new ImplPair<A, B>( a, b );
+            p = std::make_shared< ImplPair<A, B> >( a, b );
         }
-        AbstractPair* get_p() {
+
+        std::shared_ptr<AbstractPair> get_p() const {
             return p;
         }
     private:
-        AbstractPair *p;
+        std::shared_ptr<AbstractPair> p;
 };
 
 void print( std::ostream& o, std::initializer_list<Pair> lista ) {
-    for( auto elem : lista ){
-        elem.get_p()->imprime(o);
+    for( auto itr = std::begin(lista); itr != std::end(lista); ++itr ){
+        (*itr).get_p()->imprime(o);
     }
 }
 
 
 int main() {
  
-    Pair p( "1", 2 );
-
-    print( cout, { { "jan", 1 }, { 2, "fev" }, { string( "pi" ), 3.14 } } );
+    print( cout, { { "jan", 1 }, { 2, "fev" } } );
 
     return 0;  
 
